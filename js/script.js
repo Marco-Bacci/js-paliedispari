2fb206675980fa0be2848f290b463ea73037a744
Marco Bacci <marcobacci94@gmail.com>
2025-07-07 17:44:59 +0200

    feat: exercise finalised

--- js/script.js
+++ js/script.js
@@ -3,17 +3,31 @@ const word = prompt(`inserisci una parola`)
 // definisco funzione per capire se la parola è palindroma 
 palindrome(word)
 
+if(palindrome(word)===true){
+console.log("E' palindroma")
+}
+else{
+  console.log(`no palindroma.`)
+}
+
+
+
 function palindrome (text){
   let splitWord = text.split("");
   let reversedWord = splitWord.reverse();
   let jointWord = reversedWord.join("");
-  if(text === jointWord){
-    console.log(`palindroma`)
-  }
-  else{
-    console.log(`NON palindroma`)
-  }
-  
+
+  // if(text === jointWord){
+  //   console.log(`palindroma`)
+  // }
+  // else{
+  //   console.log(`NON palindroma`)
+  // }
+  return text === jointWord
 }
+
+
 // divido la parola in lettere, (split) inverto l'ordine delle lettere, (reverse) ricompongo la parola (Join)
 
+
+//2 uso risultato palindrome(word) nell'if per decidere cosa stampare in console
