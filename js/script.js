8e357764556edc9b4f9fa52b40dd09f6dea7ab53
Marco Bacci <marcobacci94@gmail.com>
2025-07-07 16:42:39 +0200

    feat: text splitted, reversed, joined

--- js/script.js
+++ js/script.js
@@ -1,10 +1,19 @@
 // dichiaro variabile parola inserita dall'utente
 const word = prompt(`inserisci una parola`)
 // definisco funzione per capire se la parola è palindroma 
-function palindrome (text){
+palindrome(word)
 
+function palindrome (text){
+  let splitWord = text.split("");
+  let reversedWord = splitWord.reverse();
+  let jointWord = reversedWord.join("");
+  if(text === jointWord){
+    console.log(`palindroma`)
+  }
+  else{
+    console.log(`NON palindroma`)
+  }
+  
 }
 // divido la parola in lettere, (split) inverto l'ordine delle lettere, (reverse) ricompongo la parola (Join)
-let inversedWord = word.split("");
 
-// il return della funzione deve essere la parola === alla parola inversa
