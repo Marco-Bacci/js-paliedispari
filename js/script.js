34cdd9f4423e5cb5a824bd18d0af6eab66fd0777
Marco Bacci <marcobacci94@gmail.com>
2025-07-07 23:45:59 +0200

    feat: function verify odd or even

--- js/script.js
+++ js/script.js
@@ -27,13 +27,37 @@
 // dichiaro variabile dchiedendo all'utente di scegliere tra pari e dispari
 const oddOrEven = prompt("scegli tra `pari` e `dispari`")
 
+// chiamo la variabile data dal return della funzione
+
+let sum = random()
+console.log(`la somma è ${sum}`)
+
+
+
+
+
+
+
+
+
+
+
+//  creo funzione che mi da la somma tra il numero scelto dall'utente e il numero del pc
 function random(){
 const userNum = parseInt(prompt("inserisci numero da 1 a 5"));
 const pcNum = Math.floor(Math.random() * 5)+ 1;
 console.log(`L'utente ha scelto ${userNum}, il pc ha scelto ${pcNum}`)
 return userNum + pcNum
 }
-
+// creo funzione che stabilisce il se la somma dei numeri è pari o dispari
+function numCheck (numbs){
+  if (numbs % 2 === 0){
+    return `pari`
+  }
+  else{
+    return `dispari`
+  }
+}
 
 
 
