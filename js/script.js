bb7fc6a12b0a1d2a0f4bb31f781033b59e6d1a14
Marco Bacci <marcobacci94@gmail.com>
2025-07-07 15:37:20 +0200

    feat: analize problem through comments

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// dichiaro variabile parola inserita dall'utente
+const word = prompt(`inserisci una parola`)
+// definisco funzione per capire se la parola è palindroma 
+function palindrome (text){
+
+}
+// divido la parola in lettere, (split) inverto l'ordine delle lettere, (reverse) ricompongo la parola (Join)
+let inversedWord = word.split("");
+
+// il return della funzione deve essere la parola === alla parola inversa
